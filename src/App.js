3c6ac62628f6d90a0f2060ff66278fe8d080b672
sharifulislm <shariful.islam.coder@gmail.com>
2022-04-08 00:07:50 +0300

    added regular expreddion Special passoward

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import app from './firebase.init';
 import Form from 'react-bootstrap/Form'
 import { Button } from 'react-bootstrap';
 import { useState } from 'react';
+const auth = getAuth(app);
 
 function App() {
 
-  const auth = getAuth(app);
+ 
+    const [error , setError]=useState('');
+  const [validated, setValidated] = useState(false);
   const [Email, setEmail]= useState('');
   const [passoword, setPassoword]= useState('');
 
@@ -22,17 +25,32 @@ const handlePasswordBlur = event => {
   setPassoword(event.target.value);
 }
 const handleFormSubmit = event => {
- 
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  if (form.checkValidity() === false) {
+   
+    event.stopPropagation();
+  }
+  if(!/(?=,*?[#?!@$%^&*-])/.test(passoword)){
+   setError('password should contain atleast one number and one special character')
+   return;
+  }
+
+  setValidated(true);
+
+
+ setError('');
   createUserWithEmailAndPassword(auth,Email,passoword)
   .then(result => {
     const user = result.user;
     console.log(user);
   } )
   .catch(error => {
- console.error(error);
+ console.log(error);
   })
 
-  event.preventDefault();
+
 }
 
   return (
@@ -40,19 +58,27 @@ const handleFormSubmit = event => {
   <h1> this is progamming </h1>
 
 <div className='w-50 mx-auto'>
-  <Form onSubmit={handleFormSubmit}>
+  <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
     <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
     <Form.Text className="text-muted">
       We'll never share your email with anyone else.
     </Form.Text>
+    <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
     <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+    <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
   </Form.Group>
+
+  <p className='text-danger'>{error}</p>
 
   <Button  variant="primary" type="submit">
     Submit
